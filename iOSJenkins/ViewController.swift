63e52bd2fd975bb41f9fb1d011398baebd965e0d
enomoto_shinchi <enomoto.shinchi@bestskip.com>
2021-05-28 17:51:31 +0900

    update

--- iOSJenkins/ViewController.swift
+++ iOSJenkins/ViewController.swift
@@ -14,6 +14,31 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func button1Click(_ sender: Any) {
+        let alertView = UIAlertController.init(title: "テストbutton1", message: "ばか", preferredStyle: .alert)
+           let alert = UIAlertAction.init(title: "ok", style: .destructive) { (UIAlertAction) in
+            print("ok")
+           }
+           let cancleAlert = UIAlertAction.init(title: "キャンセル", style: .cancel) { (UIAlertAction) in
+               print("キャンセル")
+           }
+           alertView.addAction(cancleAlert)
+           alertView.addAction(alert);
+           self.present(alertView, animated: true, completion: nil)
+        
+    }
+    
+    @IBAction func button2Click(_ sender: Any) {
+        let alertView = UIAlertController.init(title: "テストbutton2", message: "ばか", preferredStyle: .alert)
+           let alert = UIAlertAction.init(title: "ok", style: .destructive) { (UIAlertAction) in
+            print("ok")
+           }
+           let cancleAlert = UIAlertAction.init(title: "キャンセル", style: .cancel) { (UIAlertAction) in
+               print("キャンセル")
+           }
+           alertView.addAction(cancleAlert)
+           alertView.addAction(alert);
+           self.present(alertView, animated: true, completion: nil)
+    }
 }
 
