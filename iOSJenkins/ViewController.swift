65ceb8922930c2bfb59d9ec7c03a2d5e7ae7fdb2
enomoto_shinchi <enomoto.shinchi@bestskip.com>
2021-06-04 17:47:58 +0900

    update

--- iOSJenkins/ViewController.swift
+++ iOSJenkins/ViewController.swift
@@ -8,10 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myPickerView: UIPickerView!
+    @IBOutlet weak var myDatePickerView: UIDatePicker!
+    
+    let dummyList = ["Apple", "Orange", "Banana", "Mango", "Bilberry", "Blackberry"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        myPickerView.delegate = self
+        myPickerView.dataSource = self
+        myDatePickerView.datePickerMode = .date
+        
+        myDatePickerView.setDate(NSDate() as Date, animated: true)
+        myDatePickerView.minuteInterval = 5
+        myDatePickerView.minimumDate = NSDate(timeInterval: -93312000, since: NSDate() as Date) as Date
+        myDatePickerView.maximumDate = NSDate(timeIntervalSinceNow: 93312000) as Date
     }
 
     @IBAction func button1Click(_ sender: Any) {
@@ -39,6 +52,26 @@ class ViewController: UIViewController {
            alertView.addAction(cancleAlert)
            alertView.addAction(alert);
            self.present(alertView, animated: true, completion: nil)
+    }
+}
+
+extension ViewController:UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return dummyList.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+           return dummyList[row]
+       }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+    
+        myLabel.text = dummyList[row]
     }
 }
 
